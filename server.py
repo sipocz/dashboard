c9df503dc8f892dfc0d28921980504879121a6cf
sipocz <sipoczlaszlo@gmail.com>
2024-02-19 23:06:31 +0100

    Update server.py

--- server.py
+++ server.py
@@ -528,7 +528,7 @@ def login():
 def notdash():
    df = pd.DataFrame({
       'x': [1,2,3,4,5,6,7,8,9,10],
-      'Amount': [4, 1, 2, 2, 4, 5,6,7,3,1]
+      'Amount': [4, 1, 2, 2, 4, 5,3,7,3,1]
       
    })
    fig = px.line(df, x='x', y='Amount', markers=True)
